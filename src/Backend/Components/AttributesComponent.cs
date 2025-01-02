fdc1d9152716db8abf1a95b49b8e98e299b03e8f
Michael Yarichuk <myarichuk@bindecy.local>
2025-01-02 10:32:59 +0200

    chore: add basic character components

---
+++ src/Backend/Components/AttributesComponent.cs
@@ -0,0 +1,14 @@
+using RoguelikeToolkit.Entities;
+
+namespace Backend.Components;
+
+[Component(Name = "Attributes")]
+public class AttributesComponent
+{
+    public int Strength { get; set; }
+    public int Dexterity { get; set; }
+    public int Constitution { get; set; }
+    public int Intelligence { get; set; }
+    public int Wisdom { get; set; }
+    public int Charisma { get; set; }
+}
---
+++ src/Backend/Components/RaceComponent.cs
@@ -0,0 +1,13 @@
+using RoguelikeToolkit.Entities;
+
+namespace Backend.Components;
+
+[Component(Name = "Race")]
+public class RaceComponent
+{
+    public string Name { get; set; }
+    public int Speed { get; set; }       // in feet
+    public int Darkvision { get; set; }  // 60 means 60-foot darkvision
+    public Dictionary<string, int> ASIModifiers { get; set; } // { "DEX": 2, "WIS": 1 } etc.
+    public List<string> Traits { get; set; } // "Keen Senses", "Fey Ancestry"
+}
